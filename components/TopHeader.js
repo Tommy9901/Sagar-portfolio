30a4035fbff77414829740c86b6650e280cd2481
Tommy9901 <temuuujin0317@gmail.com>
2024-07-26 15:41:06 +0800

    sagar

--- components/TopHeader.js
+++ components/TopHeader.js
@@ -9,7 +9,7 @@ export function Header() {
     <div className="dark:text-[#D1D5DB] dark:bg-[#030712] md:flex py-16 px-4 md:py-24 md:px-20 md:gap-12 md:flex-row-reverse">
       <div className=" px-8 md:w-[380px] max-w-[400px] mx-auto">
         <div className=" bg-gray-200 dark:bg-[#374151] w-full mb-16 aspect-[5/6] relative md:top-[10%] md:left-[10%]">
-          <Image className="border-[white] border-8 dark:border-[#030712] relative rigth-80 object-cover w-[90%] block md:w-full h-full bottom-[5%] md:bottom-[10%] right-[-5%] md:right-[10%]" src="/images/SagarTop.png " width={240} height={280}   />
+          <Image className="border-[white] border-8 dark:border-[#030712] relative rigth-80 object-cover w-[90%] block md:w-full h-full bottom-[5%] md:bottom-[10%] right-[-5%] md:right-[10%]" src="/images/UglySagar.png " width={240} height={280}   />
         </div>
 
       </div>
